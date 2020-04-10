1bfcd4090ecd0cbb559aeab3683e3dc748ebdff2
Farsen976 <farsen.abdallahcombo@ynov.com>
2020-04-10 20:03:42 +0200

    front - feat: add felt temperature

--- front/src/components/DeviceDetails.vue
+++ front/src/components/DeviceDetails.vue
@@ -11,6 +11,7 @@
       <p>position : {{device.latitude}},{{device.longitude}}</p>
       <template v-if="lastMeasurement">
         <p>temperature : {{lastMeasurement.temperature}}°C</p>
+        <p>ressenti: {{temperatureFelt}}°C</p>
         <p>humidité : {{lastMeasurement.humidity}}%</p>
         <p>vent : {{lastMeasurement.wind}}km/h</p>
         <v-alert v-if="isStrongBreeze()" type="info">Vent un peu fort</v-alert>
@@ -34,19 +35,24 @@ export default {
   data() {
     return {
       lastMeasurement: undefined,
+      temperatureFelt: undefined,
       isStrongBreeze: () => this.lastMeasurement.wind >= 39 && this.lastMeasurement.wind <= 74,
       isStrongGale: () => this.lastMeasurement.wind >= 75 && this.lastMeasurement.wind <= 88,
       isStorm: () => this.lastMeasurement.wind >= 89
     };
   },
   mounted() {
-    if (this.device.measurements.length)
-      this.lastMeasurement = this.device.measurements[0];
+    if (this.device.measurements.length) this.lastMeasurement = this.device.measurements[0];
+    this.getFeltTemperature();
   },
   methods: {
     async refreshDevice() {
       const { data } = await this.$axios.get("/devices/" + this.device._id);
       if (data.measurements.length) this.lastMeasurement = data.measurements[0];
+    },
+    async getFeltTemperature() {
+      const { data } = await this.$axios.get(`/devices/${this.device._id}/felt-temperature`);
+      if(data.felt) this.temperatureFelt = Math.round(10*data.felt)/10;
     }
   }
 };
