01fd82c33a131617bef856b0ab41dead70444562
Goms <gomesdpro@gmail.com>
2020-04-11 01:01:27 +0200

    front - config(axios)

--- front/src/plugins/axios.js
+++ front/src/plugins/axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? "" : 'http://localhost:3000/api/',
+    baseURL: process.env.NODE_ENV === "production" ? "/api" : 'http://localhost:3000/api/',
 });
 
 export default axios;
