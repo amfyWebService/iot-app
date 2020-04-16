e2fb5082d29f8dc100e90765174ea46bfb971962
Goms <gomesdpro@gmail.com>
2020-04-16 13:00:15 +0200

    front - feat(404)

---
+++ front/src/views/PageNotFound.vue
@@ -0,0 +1,25 @@
+<template>
+<v-container style="height: 100%">
+    <v-row style="height: 100%" class="justify-center">
+        <v-col cols="12" class="text-center">
+            <h1 class="display-4">404 - tu t'es perdu gros ?</h1>
+        </v-col>
+        <v-col cols="12" class="text-center">
+            <router-link class="display-1" to="/">Page d'accueil</router-link>
+        </v-col>
+    </v-row>
+</v-container>
+
+  
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
